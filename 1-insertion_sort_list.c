a2526c25aa0458a371af87e060bfbb14d02fbb5b
Mariam <eng.mariamnabil93@gmail.com>
2023-11-17 22:17:36 -0800

    1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,9 +1,11 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending
- * order using an insertion sort algorithm
- * @list: doubly linked list of integers to be sorted
+ * insertion_sort_list -  function that sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: doubly linked list
  */
+
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *lead, *follow, *new, *temp;
@@ -11,20 +13,16 @@ void insertion_sort_list(listint_t **list)
 	if (!list || !(*list) || !((*list)->next))
 		return;
 
-	/* dance begins with 1st from house left following */
 	follow = (*list);
-	/* and next dancer to house right leading */
 	lead = (*list)->next;
 	while (lead)
 	{
 		new = lead->next;
 		while (follow && lead->n < follow->n)
 		{
-			/* lead and follow swap positions */
 			if (follow->prev)
 				follow->prev->next = lead;
 			else
-				/* if lead makes it to house left, now head */
 				*list = lead;
 			if (lead->next)
 				lead->next->prev = follow;
@@ -34,10 +32,8 @@ void insertion_sort_list(listint_t **list)
 			follow->next = temp;
 			follow->prev = lead;
 			print_list(*list);
-			/* compare next pair, flowing to house left */
 			follow = lead->prev;
 		}
-		/* lead sorted to left, new cycle starts @ right leading edge */
 		lead = new;
 		if (lead)
 			follow = lead->prev;
