f33e27bb5c7d3871f9845886c195be9cdd779d55
Esraa413 <esraaabdelwahab614@gmail.com>
2023-11-18 13:16:35 -0800

    sort

--- sort.h
+++ sort.h
@@ -41,5 +41,6 @@ void counting_sort(int *array, size_t size, int exp);
 void heap_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif
