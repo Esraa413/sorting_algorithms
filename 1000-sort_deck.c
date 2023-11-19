26da7e9fb718740673a3649f1af402bd9548cd1b
Esraa413 <esraaabdelwahab614@gmail.com>
2023-11-18 21:35:07 -0800

    sort

---
+++ 1000-sort_deck.c
@@ -0,0 +1,130 @@
+#include "deck.h"
+
+/**
+ * sort_deck - sort deck of the card
+ * @deck: doubly linked list
+ */
+ 
+void sort_deck(deck_node_t **deck)
+{
+	deck_node_t *curr;
+	size_t length;
+	deck_node_t *one, *two, *three, *four;
+
+	length = list_len_deck(*deck);
+	if (!deck || !*deck || len < 2)
+	{
+		return;
+	}
+	curr = *deck;
+	while (curr)
+	{
+		if (curr->prev && card_value(curr) < card_value(curr->prev))
+		{
+			one = curr->prev->prev;
+			two = curr->prev;
+			three = curr;
+			four = curr->next;
+
+			two->next = four;
+			if (four)
+			{
+				four->prev = two;
+			}
+			three->next = two;
+			three->prev = one;
+			if (one)
+			{
+				one->next = three;
+			}
+			else
+			{
+				*deck = three;
+			}
+			two->prev = three;
+			curr = *deck;
+			continue;
+		}
+		else
+			curr = curr->next;
+	}
+}
+
+/**
+ * card_value - returns the card value
+ * @node: card in the deck
+ *
+ * Return: Always 0
+ */
+ 
+int card_value(deck_node_t *node)
+{
+	char *val[13] = {"Ace", "2", "3", "4", "5", "6",
+		"7", "8", "9", "10", "Jack", "Queen", "King"};
+	char *kinds[4] = {"SPADE", "HEART", "CLUB", "DIAMOND"};
+	int x, ki_val = 0;
+	
+	x = 1;
+	while (x <= 13)
+	{
+		if (!_strcmp(node->card->value, val[x - 1]))
+		{
+			ki_val = x;
+		}
+		x++;
+	}
+	x = 1;
+	while (x <= 4)
+	{
+		if (!_strcmp(kinds[node->card->kind], kinds[x - 1]))
+		{
+			ki_val = ki_val + (13 * x);
+		}
+		x++;
+	}
+
+	return (ki_val);
+}
+
+/**
+ * _strcmp - compares two  the strings in sote
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: Always 0
+ */
+
+int _strcmp(const char *s1, const char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
+
+/**
+ * list_len_deck - returns length of the list
+ * @list: list head
+ *
+ * Return: Always 0
+ */
+
+size_t list_len_deck(deck_node_t *list)
+{
+	size_t length = 0;
+
+	while (list)
+	{
+		length++;
+		list = list->next;
+	}
+	return (length);
+}
---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,70 @@
+#include "sort.h"
+
+int partition(int *array, int low, int high);
+void quick_sort_recursive(int *array, int low, int high);
+
+/**
+* quick_sort_hoare - sort array using the Quick sort algorithm
+* @array: array of ints to sorted
+* @size: size of array
+*/
+
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	
+	quick_sort_recursive(array, 0, (int)size - 1);
+}
+
+/**
+* partition - Hoare partition
+* @array: array to  the partition
+* @low: str index of partition
+* @high: ending index of partition
+* Return: Return: Always 0
+*/
+
+int partition(int *array, int low, int high)
+{
+	int piv = array[high];
+	int x = low - 1, k = high + 1;
+	int tmp = array[x];
+
+	while (1)
+	{
+		do {
+			x++;
+		} while (array[x] < piv);
+		
+		do {
+			k--;
+		} while (array[k] > piv);
+		
+		if (x >= k)
+		{
+			return (k);
+		}
+		
+		array[x] = array[k];
+		array[k] = tmp;
+		print_array(array, (size_t)k + 1);
+	}
+}
+
+/**
+* quick_sort_recursive -  function to recursive perform Quick
+* @array: sort array
+* @low: str index of subarray
+* @high: ending index subarray
+*/
+
+void quick_sort_recursive(int *array, int low, int high)
+{
+	if (low < high)
+	{
+		int pivot_index = partition(array, low, high);
+		quick_sort_recursive(array, low, pivot_index);
+		quick_sort_recursive(array, pivot_index + 1, high);
+	}
+}
