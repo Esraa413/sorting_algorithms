a451110a95ce59e53f527bf84df56327973a29d3
Esraa413 <esraaabdelwahab614@gmail.com>
2023-11-18 22:19:40 -0800

    sort

--- deck.h
+++ deck.h
@@ -4,12 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * enum kind_e - the kids of cards
+ * @SPADE: the spade of cards
+ * @HEART: the heart of cards
+ * @CLUB: the club of cards
+ * @DIAMOND: the diamond  of cards
+ */
+
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -21,8 +29,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -34,9 +42,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
