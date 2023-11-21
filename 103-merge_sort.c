0dfe7586dd0550a9a5ebf2a13f2f54b659876b3f
Mariam <eng.mariamnabil93@gmail.com>
2023-11-20 20:59:41 -0800

    1

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,69 +1,110 @@
 #include "sort.h"
-
+#include <stdio.h>
 /**
-* merge_sort - sorts an array with the Merge Sort algorithm
-* @array: array of integers to sort
-* @size: size of the array
-*/
+ *_calloc - this is a calloc function
+ *@nmemb: number of elemets
+ *@size: bit size of each element
+ *Return: pointer to memory assignement
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return ('\0');
+	p = malloc(nmemb * size);
+	if (p == '\0')
+		return ('\0');
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = '\0';
+	return (p);
+}
+/**
+ *merge - make a merge
+ *@arr: one from start to mid
+ *@tmp: temp array used in merge, was created outside to
+ *optimize reducing the system calls
+ *@start: first element position
+ *@mid: array middle
+ *@end: last element position
+ **/
+void merge(int *arr, int *tmp, int start, int mid, int end)
+{
+	/*  sizes and temp arrays */
+	int size_left = mid - start + 1, size_right = end - mid;
+	int *array_left, *array_right;
+	/* counters */
+	int left, right, i = 0;
+
+	array_left = &tmp[0];
+	array_right = &tmp[size_right];
+	for (left = 0; left < size_left; left++)
+		array_left[left] = arr[start + left];
+	for (right = 0; right < size_right; right++)
+		array_right[right] = arr[mid + 1 + right];
+	left = 0, right = 0, i = start;
+	/* merging tmp arrays into main array*/
+	while (left < size_left && right < size_right)
+	{
+		if (array_left[left] <= array_right[right])
+			arr[i] = array_left[left], left++;
+		else
+			arr[i] = array_right[right], right++;
+		i++;
+	}
+	/* merging remaining left array into main array*/
+	while (left < size_left)
+		arr[i] = array_left[left], left++, i++;
+	/* merging remaining right array into main array*/
+	while (right < size_right)
+		arr[i] = array_right[right], right++, i++;
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array_left, left);
+	printf("[right]: ");
+	print_array(array_right, right);
+	printf("[Done]: ");
+	print_array(&arr[start], left + right);
+}
+/**
+ *mergesort - function that sorts an array of integers
+ *in ascending order using the Merge sort algorithm
+ *@array: array of integers
+ *@tmp: temp array used in merge, was created outside to
+ *optimize reducing the system calls
+ *@start: fisrt element position
+ *@end: last element position
+ *Return: void
+ */
+void mergesort(int *array, int *tmp, int start, int end)
+{
+	int mid;
+
+	mid = (start + end) / 2;
+	if ((start + end) % 2 == 0)
+		mid--;
+	if (mid >= start)
+	{
+		mergesort(array, tmp, start, mid);
+		mergesort(array, tmp, mid + 1, end);
+		merge(array, tmp, start, mid, end);
+	}
+}
+/**
+ *merge_sort - function that sorts an array of integers
+ *in ascending order using the Merge sort algorithm
+ *@size: size of the list
+ *@array: array of integers
+ *Return: void
+ */
 void merge_sort(int *array, size_t size)
 {
-int *temp_array;
+	int *tmp;
 
-if (!array || size < 2)
-return;
-
-temp_array = malloc(sizeof(int) * size);
-
-merge_sort_recursive(temp_array, array, 0, size);
-free(temp_array);
-}
-
-/**
-* merge_subarray - merges subarrays
-* @temp: temporary array for merging
-* @array: array to merge
-* @left: index of the left element
-* @middle: index of the middle element
-* @right: index of the right element
-*/
-void merge_subarray(int *temp, int *array, size_t left,
-size_t middle, size_t right)
-{
-size_t i, j, k = 0;
-
-for (i = left, j = middle; i < middle && j < right; k++)
-{
-if (array[i] < array[j])
-temp[k] = array[i++];
-else
-temp[k] = array[j++];
-}
-
-while (i < middle)
-temp[k++] = array[i++];
-while (j < right)
-temp[k++] = array[j++];
-
-for (k = left, i = 0; k < right; k++)
-array[k] = temp[i++];
-}
-
-/**
-* merge_sort_recursive - recursive function that merge sorts an array
-* @temp: temporary array for merging
-* @array: array to merge sort
-* @left: index of the left element
-* @right: index of the right element
-*/
-void merge_sort_recursive(int *temp, int *array, size_t left, size_t right)
-{
-size_t middle;
-
-if (right - left > 1)
-{
-middle = (right - left) / 2 + left;
-merge_sort_recursive(temp, array, left, middle);
-merge_sort_recursive(temp, array, middle, right);
-merge_subarray(temp, array, left, middle, right);
-}
+	if (!array || size < 2)
+		return;
+	tmp = _calloc(size, sizeof(int));
+	mergesort(array, tmp, 0, size - 1);
+	free(tmp);
 }
